346b57146792ddf9c0c16bac038e71c2f51c53ee
Farhad Shikder <frd1952@gmail.com>
2021-08-20 19:01:12 +0600

    Laravel collective form installed and added user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
         User::create([
            'name' => 'Farhad Shikder',
            'email' => 'admin@gmail.com',
